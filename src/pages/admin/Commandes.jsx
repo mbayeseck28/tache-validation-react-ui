0c457c7216d508659a90f7e56afdfcb41ccaa56c
Awa_la_princesse <miss-ndeya>
2024-02-01 10:06:22 +0000

    sidebare

---
+++ src/pages/admin/Commandes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Commandes = () => {
+  return (
+    <div className='m-3 text-xl font-bold underline'>
+      Commandes
+    </div>
+  )
+}
+
+export default Commandes
