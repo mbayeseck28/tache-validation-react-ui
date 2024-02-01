3367abf2aa235f6242fb228466833231814bc0d8
Moussa Diatta <diattamoussa010@gmail.com>
2024-02-01 22:06:08 +0000

    recuperation des graphes

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Cards from '../../components/cards-et-filtre/Cards'
 import Filtre from '../../components/cards-et-filtre/Filtre'
+import Revenue from './Revenue'
 
 const Dashboard = () => {
   return (
-    <div className='m-3 text-xl container font-bold '>
-      <div className='flex justify-between'>
-      <div>Dashboard</div>
-      <Filtre/>
+    <div>
+      <div className='m-3 text-xl container font-bold '>
+        <div className='flex justify-between'>
+        <div>Dashboard</div>
+        <Filtre/>
+        </div>
+        <Cards/>
       </div>
-      
-
-      <Cards/>
+      <div className='lg:grid-cols-6 md:grid-cols-12'>
+        <Revenue />
+      </div>
     </div>
   )
 }
