9c1dcef86e6d85197587955dba60545297cb813c
BAMoustapha <bam480836@gmail.com>
2024-02-01 17:37:33 +0000

    ba

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import ListeProduits from '../../components/listeproduits/ListeProduits'
 
 const Dashboard = () => {
+  
   return (
+    <>
     <div className='m-3 text-xl font-bold underline'>
       Dashboard
-    </div>
+      </div>
+      <ListeProduits />
+    </>
   )
 }
 
